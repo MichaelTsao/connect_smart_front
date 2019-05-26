92f262b416d42a4948246ddccba5ed3555d45f6f
caoxiang <caoxiang@yeah.net>
2019-05-26 20:31:09 +0800

    real begin

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,8 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+          <span>人脉挖掘</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
@@ -22,9 +13,9 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+    import HelloWorld from './components/HelloWorld'
 
-export default {
+    export default {
   name: 'App',
   components: {
     HelloWorld
