7b229fdabe4f34803fa0e8b6d3371fd91d273618
caoxiang <caoxiang@yeah.net>
2019-06-03 18:27:59 +0800

    use api get data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -39,7 +39,7 @@
                                     <v-text-field label="Carbs (g)" v-model="editedItem.carbs"></v-text-field>
                                 </v-flex>
                                 <v-flex md4 sm6 xs12>
-                                    <v-text-field label="Protein (g)" v-model="editedItem.protein"></v-text-field>
+                                    <v-text-field label="Protein (g)" v-model="editedItem.company"></v-text-field>
                                 </v-flex>
                             </v-layout>
                         </v-container>
@@ -56,7 +56,7 @@
 
         <v-data-table
                 :headers="headers"
-                :items="desserts"
+                :items="friends"
                 class="elevation-1"
         >
             <template v-slot:items="props">
@@ -64,7 +64,7 @@
                 <td class="text-xs-right">{{ props.item.calories }}</td>
                 <td class="text-xs-right">{{ props.item.fat }}</td>
                 <td class="text-xs-right">{{ props.item.carbs }}</td>
-                <td class="text-xs-right">{{ props.item.protein }}</td>
+                <td class="text-xs-right">{{ props.item.company }}</td>
                 <td class="justify-center layout px-0">
                     <v-icon
                             @click="editItem(props.item)"
@@ -102,24 +102,24 @@
                 {text: '人脉强度', value: 'calories', align: 'right'},
                 {text: '熟识度', value: 'fat', align: 'right'},
                 {text: '影响力', value: 'carbs', align: 'right'},
-                {text: '行业', value: 'protein', align: 'right'},
+                {text: '公司', value: 'company', align: 'right'},
                 {text: '操作', value: 'name', sortable: false, align: 'center'}
             ],
-            desserts: [],
+            friends: [],
             editedIndex: -1,
             editedItem: {
                 name: '',
                 calories: 0,
                 fat: 0,
                 carbs: 0,
-                protein: 0
+                company: 0
             },
             defaultItem: {
                 name: '',
                 calories: 0,
                 fat: 0,
                 carbs: 0,
-                protein: 0
+                company: 0
             }
         }),
 
@@ -141,89 +141,29 @@
 
         methods: {
             initialize() {
-                this.desserts = [
-                    {
-                        name: 'Frozen Yogurt',
-                        calories: 159,
-                        fat: 6.0,
-                        carbs: 24,
-                        protein: 4.0
-                    },
-                    {
-                        name: 'Ice cream sandwich',
-                        calories: 237,
-                        fat: 9.0,
-                        carbs: 37,
-                        protein: 4.3
-                    },
-                    {
-                        name: 'Eclair',
-                        calories: 262,
-                        fat: 16.0,
-                        carbs: 23,
-                        protein: 6.0
-                    },
-                    {
-                        name: 'Cupcake',
-                        calories: 1305,
-                        fat: 3.7,
-                        carbs: 67,
-                        protein: 4.3
-                    },
-                    {
-                        name: 'Gingerbread',
-                        calories: 356,
-                        fat: 16.0,
-                        carbs: 49,
-                        protein: 3.9
-                    },
-                    {
-                        name: 'Jelly bean',
-                        calories: 375,
-                        fat: 0.0,
-                        carbs: 94,
-                        protein: 0.0
-                    },
-                    {
-                        name: 'Lollipop',
-                        calories: 392,
-                        fat: 0.2,
-                        carbs: 98,
-                        protein: 0
-                    },
-                    {
-                        name: 'Honeycomb',
-                        calories: 408,
-                        fat: 3.2,
-                        carbs: 87,
-                        protein: 6.5
-                    },
-                    {
-                        name: 'Donut',
-                        calories: 452,
-                        fat: 25.0,
-                        carbs: 51,
-                        protein: 4.9
-                    },
-                    {
-                        name: 'KitKat',
-                        calories: 518,
-                        fat: 26.0,
-                        carbs: 65,
-                        protein: 7
-                    }
-                ]
+                var that = this;
+                const http = require('axios');
+                http.get('http://cs.cx/friends?access-token=100-token')
+                    .then(function (response) {
+                        // handle success
+                        that.friends = response.data;
+                        console.log(response.data);
+                    })
+                    .catch(function (error) {
+                        // handle error
+                        console.log(error);
+                    });
             },
 
             editItem(item) {
-                this.editedIndex = this.desserts.indexOf(item)
+                this.editedIndex = this.friends.indexOf(item)
                 this.editedItem = Object.assign({}, item)
                 this.dialog = true
             },
 
             deleteItem(item) {
-                const index = this.desserts.indexOf(item)
-                confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+                const index = this.friends.indexOf(item)
+                confirm('Are you sure you want to delete this item?') && this.friends.splice(index, 1)
             },
 
             close() {
@@ -236,9 +176,9 @@
 
             save() {
                 if (this.editedIndex > -1) {
-                    Object.assign(this.desserts[this.editedIndex], this.editedItem)
+                    Object.assign(this.friends[this.editedIndex], this.editedItem)
                 } else {
-                    this.desserts.push(this.editedItem)
+                    this.friends.push(this.editedItem)
                 }
                 this.close()
             }
