a1e5486eb268395ab1991268a761e239527aa205
caoxiang <caoxiang@yeah.net>
2019-07-15 16:15:28 +0800

    引入echarts库并制作饼图

--- src/components/Stat.vue
+++ src/components/Stat.vue
@@ -11,16 +11,18 @@
                     :key="content.name"
                     v-for="content in contents"
             >
-                <v-sheet
-                        class="d-flex"
-                        :color='content.color+" lighten-3"'
-                        height="424"
-                        elevation=8
-                >
-                    <sheet-footer>
-                        {{content.name}}
-                    </sheet-footer>
-                </v-sheet>
+                <v-card>
+                    <div
+                            :id="'pie-'+content.type"
+                            style="width: 350px; height:350px; margin-top: 25px; margin-left: 15px"
+                    ></div>
+
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">{{ content.name }}</h3>
+                        </div>
+                    </v-card-title>
+                </v-card>
             </v-flex>
 
 
@@ -30,32 +32,59 @@
 
 <script>
     export default {
+        name: "Stat",
+
         data: () => ({
             contents: [
-                {name: 'aa', color: 'green'},
-                {name: 'ab', color: 'yellow'},
-                {name: 'ac', color: 'red'},
-                {name: 'ad', color: 'teal'},
-                {name: 'ae', color: 'purple'},
+                {name: '关系点', type: 'relation'},
+                {name: '行业', type: 'industry'},
+                {name: '性别', type: 'gender'},
+                {name: '地区', type: 'location'},
             ],
         }),
-        name: "Stat",
-        components: {
-            SheetFooter: {
-                functional: true,
 
-                render(h, {children}) {
-                    return h('v-sheet', {
-                        staticClass: 'mt-auto align-center justify-center d-flex',
-                        props: {
-                            color: 'rgba(0, 0, 0, .36)',
-                            dark: true,
-                            height: 50
-                        }
-                    }, children)
+        mounted() {
+            this.drawCharts();
+        },
+
+        methods: {
+            drawCharts() {
+                let echarts = require('echarts');
+                const http = require('axios');
+
+                for (let j in this.contents) {
+                    let type = this.contents[j].type;
+                    http.get(process.env.VUE_APP_API_URL + '/friends/groupby/' + type + '?access-token=100-token')
+                        .then(function (response) {
+                            let data = [];
+                            for (let i in response.data) {
+                                data.push({value: response.data[i].count, name: response.data[i]._id})
+                            }
+
+                            let myChart = echarts.init(document.getElementById('pie-' + type));
+                            let option = {
+                                tooltip: {
+                                    trigger: 'item',
+                                    formatter: "{a} <br/>{b} : {c} ({d}%)"
+                                },
+                                series: [{
+                                    type: 'pie',
+                                    name: '关系点',
+                                    radius: '55%',
+                                    data: data,
+                                }]
+                            };
+
+                            // 使用刚指定的配置项和数据显示图表。
+                            myChart.setOption(option);
+                            // console.log(response);
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
                 }
-            }
-        }
+            },
+        },
     }
 </script>
 
