0f69a7a4cd262b9dc581321fe76c2c9429f98080
caoxiang <caoxiang@yeah.net>
2019-07-29 00:15:42 +0800

    增加新建连接按钮

--- src/App.vue
+++ src/App.vue
@@ -15,20 +15,48 @@
                 <v-icon>forum</v-icon>
             </v-btn>
 
-            <v-fab-transition>
-                <v-btn
-                        absolute
-                        color="pink"
-                        dark
-                        fab
-                        left
-                        style="margin-top: 100px"
-                        top
-                        v-show="show_add_button"
-                >
-                    <v-icon>add</v-icon>
-                </v-btn>
-            </v-fab-transition>
+            <v-dialog max-width="1000px" v-model="dialog">
+                <template v-slot:activator="{ on }">
+                    <v-fab-transition>
+                        <v-btn
+                                absolute
+                                color="pink"
+                                dark
+                                fab
+                                left
+                                style="margin-top: 100px"
+                                top
+                                v-on="on"
+                                v-show="show_add_button"
+                        >
+                            <v-icon>add</v-icon>
+                        </v-btn>
+                    </v-fab-transition>
+                </template>
+                <v-card>
+                    <v-card-title>
+                        <span class="headline">abc</span>
+                    </v-card-title>
+
+                    <v-card-text>
+                        <v-container grid-list-md>
+                            <v-layout wrap>
+                                <v-flex md3 sm6 xs12>
+                                    <v-text-field
+                                            label="名字"
+                                    ></v-text-field>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" flat>关闭</v-btn>
+                        <v-btn color="blue darken-1" flat>保存</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
         </v-toolbar>
 
         <v-content>
